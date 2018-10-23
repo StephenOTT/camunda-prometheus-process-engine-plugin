02f89615fe4f53729fa72d393d68b9fd85cc8792
StephenOTT <stephenrussett@gmail.com>
2018-10-23 15:50:50 -0400

    Added optimizations for activity duration lookup to use DbEntityCache

--- src/main/java/io/digitalstate/camunda/prometheus/parselisteners/executionlisteners/ActivityDurationExecutionListener.java
+++ src/main/java/io/digitalstate/camunda/prometheus/parselisteners/executionlisteners/ActivityDurationExecutionListener.java
@@ -4,13 +4,12 @@ import io.digitalstate.camunda.prometheus.collectors.SimpleHistogramMetric;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
-import org.camunda.bpm.engine.history.HistoricActivityInstance;
 import org.camunda.bpm.engine.impl.cfg.TransactionState;
 import org.camunda.bpm.engine.impl.context.Context;
+import org.camunda.bpm.engine.impl.history.event.HistoricActivityInstanceEventEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,16 +17,16 @@ public class ActivityDurationExecutionListener implements ExecutionListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ActivityDurationExecutionListener.class);
 
-    public String histogramName = "activity_instance_duration";
-    public String histogramHelp = "the duration of activity instances";
-    public List<String> histogramLabelNames = Arrays.asList("engine_name", "element_type", "process_definition_id", "activity_id");
-    public List<Double> histogramBuckets = null;
-    public Boolean useProcessDefinitionIdWithName = false;
+    private String histogramName = "activity_instance_duration";
+    private String histogramHelp = "the duration of activity instances";
+    private List<String> histogramLabelNames = Arrays.asList("engine_name", "element_type", "process_definition_id", "activity_id");
+    private List<Double> histogramBuckets = null;
+    private Boolean useProcessDefinitionIdWithName = false;
 
-    public void notify(DelegateExecution execution) {
+    public void notify(DelegateExecution execution) throws Exception {
         final String processDefinitionId = execution.getProcessDefinitionId();
-        final String processInstanceId = execution.getProcessInstanceId();
         final String activityInstanceId = execution.getActivityInstanceId();
+        final String activityId = execution.getCurrentActivityId();
         final String elementTypeName = execution.getBpmnModelElementInstance().getElementType().getTypeName();
 
         ProcessEngine engine = (ProcessEngine) execution.getProcessEngineServices();
@@ -35,40 +34,64 @@ public class ActivityDurationExecutionListener implements ExecutionListener {
 
         // If boolean is set to true for useProcessDefinitionkeyWithName, then we modify the metric name
         String histogramNameAggregate;
-        if (useProcessDefinitionIdWithName){
+        if (this.useProcessDefinitionIdWithName){
             histogramNameAggregate = String.format("%s_%s", histogramName, processDefinitionId);
         } else {
-            histogramNameAggregate = histogramName;
+            histogramNameAggregate = this.histogramName;
         }
 
-        // Will create a new Metric or return the existing metric if they share a name
+        // Will create a new Metric or return the existing metric if the name already exists in the histogram map.
+        //@TODO consider rebuilding this metric into the core prometheus api rather than Simple Metric
+        //@TODO simple metric has extra overhead for business friendly which is not required here.
         SimpleHistogramMetric histogramMetric = new SimpleHistogramMetric(histogramNameAggregate,
-                histogramHelp,
-                histogramBuckets,
-                histogramLabelNames);
+                this.histogramHelp,
+                this.histogramBuckets,
+                this.histogramLabelNames);
 
+        // Adds a transaction listener into the current transaction so we can grab the activity duration once it has been committed.
+        Context.getCommandContext().getTransactionContext()
+                .addTransactionListener(TransactionState.COMMITTED, commandContext -> {
 
-        if (ExecutionListener.EVENTNAME_END.equals(execution.getEventName())){
-            // Adds a transaction into the current transaction so we can grab the activity duration.
-            Context.getCommandContext().getTransactionContext()
-                    .addTransactionListener(TransactionState.COMMITTED, commandContext -> {
-                        // @TODO Review the usage of the command context for possible better way to access the activity instance
-                        HistoricActivityInstance activity = execution.getProcessEngineServices()
-                                .getHistoryService().createHistoricActivityInstanceQuery()
-                                .activityInstanceId(activityInstanceId)
-                                .processInstanceId(processInstanceId)
-                                .singleResult();
+                    // Get the cached entity which holds the duration of the activity instance
+                    // Cache is used so another call to the DB is not required, as this chunk of
+                    // code may be run for every single activity execution.
+                    HistoricActivityInstanceEventEntity activity = commandContext.getDbEntityManager()
+                            .getCachedEntity(HistoricActivityInstanceEventEntity.class, activityInstanceId);
 
-                        if (activity != null){
-                            long duration = activity.getDurationInMillis();
+                    if (activity != null){
+                        long duration = activity.getDurationInMillis();
 
-                            histogramMetric.observeValue(duration, Arrays.asList(engineName, elementTypeName, processDefinitionId, activityInstanceId));
-                            LOGGER.debug("Prometheus Activity Duration collected: {} : {} : {}", processDefinitionId,  activityInstanceId, duration);
+                        // set the histogram with the duration from the activity instance
+                        histogramMetric.observeValue(duration, Arrays.asList(engineName, elementTypeName, processDefinitionId, activityId));
+                        LOGGER.debug("Prometheus Activity Duration collected: {} : {} : {}", processDefinitionId,  activityInstanceId, duration);
 
-                        } else {
-                            LOGGER.debug("Activity Instance query returned null: {} : {}", processDefinitionId,  activityInstanceId);
-                        }
-                    });
-        }
+                    } else {
+                        LOGGER.debug("Activity Instance Entity query returned null: {} : {}", processDefinitionId,  activityInstanceId);
+                    }
+                });
+    }
+
+    //
+    // GETTERS AND SETTERS
+    //
+
+    public void setHistogramName(String histogramName) {
+        this.histogramName = histogramName;
+    }
+
+    public void setHistogramHelp(String histogramHelp) {
+        this.histogramHelp = histogramHelp;
+    }
+
+    public void setHistogramLabelNames(List<String> histogramLabelNames) {
+        this.histogramLabelNames = histogramLabelNames;
+    }
+
+    public void setHistogramBuckets(List<Double> histogramBuckets) {
+        this.histogramBuckets = histogramBuckets;
+    }
+
+    public void setUseProcessDefinitionIdWithName(Boolean useProcessDefinitionIdWithName) {
+        this.useProcessDefinitionIdWithName = useProcessDefinitionIdWithName;
     }
 }
